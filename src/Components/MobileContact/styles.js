bb1741f5da5c57a0ab9611a4678412f65018a86d
Vasileios Mpletsos <msa17020@gmail.com>
2025-07-06 00:22:25 +0300

    Start fresh on new project to deploy on Azure Static Web App

--- src/Components/MobileContact/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Call = styled.div`
-    position: fixed;
-    bottom: 20px;
-    right: 30px;
-    display: ${props => props.show ? "block" : "none"};
-` ;
---
+++ src/pages/index.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Vasilis Mpletsos CV</title>
+        <meta name="description" content="This is my personal site to show my CV online." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
+      >
+        <main className={styles.main}>
+          <ol>
+            <li>
+              Create your first CV Site on Azure!
+            </li>
+          </ol>
+        </main>
+      </div>
+    </>
+  );
+}
